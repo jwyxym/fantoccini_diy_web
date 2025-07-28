10744782d1bb173f2dbe3ea68262a4833411966e
jwyxym <jwyxym@126.com>
2025-07-28 12:36:05 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use fantoccini::{
+    Client, Locator,
+    elements::{
+        Element
+    }
+};
+use anyhow::Result;
+use reqwest::Client as ReqwestClient;
+use std::fs::File;
+use std::io::Write;
+use std::time::Duration;
+use std::thread;
+use std::path::{Path, PathBuf};
+async fn print(client: Client) -> Result<Client> {
+    let text = client.find(Locator::Css("body")).await?.text().await?;
+    println!("{}", text);
+    Ok(client)
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let client: Client = Client::new("http://localhost:4444").await?;
+    
+    client.goto("http://localhost:5173/").await?;
+    let client: Client = print(client).await?;
+    thread::sleep(Duration::from_secs(2));
+    let elements:Vec<Element>  = client.find_all(Locator::Css(".pic")).await?;
+    fs::create_dir_all("downloads")?;
+
+    for i in elements.iter() {
+        let src = i.text().await?;
+        let name = i.attr("id").await.unwrap_or_default();
+        match name {
+            Some(N) => {
+                println!("{}", &N);
+                let bytes = base64::decode(src)?;
+                let mut path = PathBuf::new();
+                path.push("downloads");
+                path.push(format!("{}.jpg", N));
+                std::fs::write(path, bytes)?;
+            }
+            None => {}
+        }
+    }
+        
+    client.close().await?;
+    Ok(())
+}
