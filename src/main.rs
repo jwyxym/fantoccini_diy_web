394d72c7fcc9da367412493cb91790bcc108d481
jwyxym <jwyxym@126.com>
2025-07-28 18:00:01 +0800

    fix

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,13 @@ use fantoccini::{
     }
 };
 use anyhow::Result;
-use reqwest::Client as ReqwestClient;
-use std::fs::File;
-use std::io::Write;
-use std::time::Duration;
-use std::thread;
-use std::path::{Path, PathBuf};
-async fn print(client: Client) -> Result<Client> {
-    let text = client.find(Locator::Css("body")).await?.text().await?;
-    println!("{}", text);
-    Ok(client)
-}
-use std::fs;
+use std::{
+    path::PathBuf,
+    fs,
+    thread::sleep,
+    time::Duration
+};
+use rusqlite::Connection;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -24,27 +19,37 @@ async fn main() -> Result<()> {
     let client: Client = Client::new("http://localhost:9515").await?;
     
     client.goto("http://localhost:5173/").await?;
-    let client: Client = print(client).await?;
-    thread::sleep(Duration::from_secs(2));
-    let elements:Vec<Element>  = client.find_all(Locator::Css(".pic")).await?;
+    let conn: Connection = Connection::open("cards.cdb")?;
+    let count: u64 = conn.query_row(
+        "SELECT COUNT(*) FROM datas JOIN texts ON datas.id = texts.id",
+        [],
+        |row| row.get(0),
+    )?;
+    println!("{}", count);
     fs::create_dir_all("downloads")?;
-    println!("{}", elements.len());
-
-    for i in elements.iter() {
-        let src = i.text().await?;
-        let name = i.attr("id").await.unwrap_or_default();
-        match name {
-            Some(N) => {
-                println!("{}", &N);
-                let bytes = base64::decode(src)?;
-                let mut path = PathBuf::new();
-                path.push("downloads");
-                path.push(format!("{}.jpg", N));
-                std::fs::write(path, bytes)?;
+    loop {
+        let elements:Vec<Element>  = client.find_all(Locator::Css(".pic")).await?;
+        if count == elements.len() as u64 {
+            for i in elements.iter() {
+                let src = i.text().await?;
+                let name = i.attr("id").await.unwrap_or_default();
+                match name {
+                    Some(N) => {
+                        println!("{}", &N);
+                        let bytes = base64::decode(src)?;
+                        let mut path = PathBuf::new();
+                        path.push("downloads");
+                        path.push(format!("{}.jpg", N));
+                        std::fs::write(path, bytes)?;
+                    }
+                    None => {}
+                }
             }
-            None => {}
+            break;
         }
+        sleep(Duration::from_secs(1));
     }
+
         
     client.close().await?;
     Ok(())
